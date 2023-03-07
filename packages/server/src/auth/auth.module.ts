ea1f7b78ee26f9d2a8229841864629a171cfe09e
cbolles <collinbolles@gmail.com>
2023-03-07 14:44:24 -0500

    Add in basic auth

---
+++ packages/server/src/auth/auth.module.ts
@@ -0,0 +1,26 @@
+import { JwtModule } from '@nestjs/jwt';
+import { forwardRef, Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
+import { JwtAuthGuard } from './jwt.guard';
+import { AuthService } from './auth.service';
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [forwardRef(() => AuthModule)],
+      inject: [AuthService],
+      useFactory: async (authService: AuthService) => {
+        return {
+          publicKey: await authService.getPublicKey(),
+          signOptions: {
+            algorithm: 'RS256',
+          }
+        };
+      }
+    })
+  ],
+  providers: [AuthService, JwtStrategy, JwtAuthGuard]
+})
+export class AuthModule {}
