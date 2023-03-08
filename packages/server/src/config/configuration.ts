df448a58fe4571cccc19c0e4d69e933eaab89911
cbolles <collinbolles@gmail.com>
2023-03-08 14:03:17 -0500

    Add basic signing module

--- packages/server/src/config/configuration.ts
+++ packages/server/src/config/configuration.ts
@@ -1,5 +1,10 @@
 export default () => ({
   mongo: {
     uri: process.env.MONGO_URI || 'mongodb://localhost:27017/cargo'
+  },
+  s3: {
+    accessKeyId: process.env.S3_ACCESS,
+    secretAccessKey: process.env.S3_SECRET,
+    region: process.env.S3_REGION
   }
 });
