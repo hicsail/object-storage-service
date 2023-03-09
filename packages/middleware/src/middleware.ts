c1eff0b06cff3c05969e94db475fc01186af26a7
cbolles <collinbolles@gmail.com>
2023-03-09 13:21:13 -0500

    Working full cycle middleware

--- packages/middleware/src/middleware.ts
+++ packages/middleware/src/middleware.ts
@@ -1,5 +1,7 @@
 import { awsAuthMiddlewareOptions } from '@aws-sdk/middleware-signing';
 import { S3MiddlewareType, S3MiddlewareStack } from './s3types';
+import { ApolloClient, InMemoryCache, HttpLink, gql } from '@apollo/client/core';
+import fetch from 'cross-fetch';
 
 export interface CargoMiddlewareConfig {
   /** The Cargo Server to sign against */
@@ -9,10 +11,29 @@ export interface CargoMiddlewareConfig {
 /**
  * Make the middleware based on the provided configuration
  */
-const makeMiddleware: (config: CargoMiddlewareConfig) => S3MiddlewareType = (_config) => {
+const makeMiddleware: (config: CargoMiddlewareConfig) => S3MiddlewareType = (config) => {
+  const apolloClient = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: new HttpLink({ uri: config.cargoEndpoint, fetch })
+  });
+
   // TODO: Determing typing for next and args
   return (next: any) => async (args: any) => {
     console.log('hello');
+
+    const query = gql`
+      query signRequest($request: ResourceRequest!) {
+        signRequest(request: $request) {
+          signature,
+          bodyHash
+        }
+      }`;
+
+    const response = await apolloClient.query({ query, variables: { request: args.request } });
+    const { signature, bodyHash } = response.data.signRequest;
+
+    args.request['headers']['authorization'] = signature;
+    args.request['headers']['x-amz-content-sha256'] = bodyHash;
 
     // Do something with the args
     return next(args);
