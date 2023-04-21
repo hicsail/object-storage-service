dddf98bf0027e39453b50db0ea9cc33c9fbc4062
cbolles <collinbolles@gmail.com>
2023-04-21 16:40:41 -0400

    Remove need for credentials in environment variables

--- packages/server/src/sign/sign.module.ts
+++ packages/server/src/sign/sign.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SignResolver } from './sign.resolver';
 import { SignService } from './sign.service';
 import { PermsModule } from '../perms/perms.module';
+import { CargoAccountModule } from '../account/account.module';
 
 @Module({
-  imports: [PermsModule],
+  imports: [PermsModule, CargoAccountModule],
   providers: [SignService, SignResolver]
 })
 export class SignModule {}
